c3bdb334f073a2ee27257c93be7182b142e783bb
Mateus Meneses Rocha <contato.menesesdev@gmail.com>
2023-06-03 03:31:35 -0300

    Adicionando as imagens

--- lib/widgets/vidente_app.dart
+++ lib/widgets/vidente_app.dart
@@ -1,11 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:previsao_do_tempo/widgets/home.dart';
 
 class VidenteApp extends StatelessWidget {
   const VidenteApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
   }
 }
